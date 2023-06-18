def816472bfaf8252b6099f828a39bc2786e314c
Duka <solomko2020@yandex.ru>
2023-06-18 15:16:54 +0300

    Написал первый запуск бота и подключение к нему через токен

--- bot.py
+++ bot.py
@@ -1,2 +1,12 @@
 # first aiogram project on Python with Git and GitHub
-import aiogram
+from aiogram import Bot, Dispatcher, executor, types
+#import requests
+
+# https://api.telegram.org/bot<token>/METHOD_NAME
+TOKEN_API = '6036510465:AAFBx2_YguG4K7XtDsW23lvlRRDx7OIBkag' # Токен для подключения к telegram API
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+if __name__ == '__main__':
+    executor.start_polling(dp) # Запускаем бота
