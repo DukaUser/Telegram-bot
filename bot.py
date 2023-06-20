feab57bfb44171ceb071c58df82c705eea9e35b9
Duka <solomko2020@yandex.ru>
2023-06-21 01:09:28 +0300

    удалил ненужное

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from random import choice
 types - Помогает писать анотации в функциях
 Dispatcher - Улавливает события в чате или события отправки сообщения боту
 executor -  Для запуска бота в онлайн
+choice - Из списка/кортежа задает рандомное значение
 '''
 
 
@@ -26,7 +27,7 @@ f.close()
 
 # Вход в систему использования ботом
 _id = (985508783,) # Мой id в телеграм, то есть все пользователи кому доступен бот
-admin_only = lambda message: message.from_user.id not in _id # Анонимная функция которая выдаёт bool значение при проверки является ли пользователь в acl
+admin_only = lambda message: message.from_user.id not in _id # Анонимная функция которая выдаёт bool значение при проверки является ли пользователь в _id
 @dp.message_handler(admin_only, content_types=['any']) # проверяет если хотябы один из кортежа является True выдаёт доступ
 async def handle_unwanted_users(message: types.Message): # функция которая удаляет нежелательных пользователей сообщения
     await bot.delete_message(message.chat.id, message.message_id) # await ждёт пока в потоке появится свободное место
@@ -50,10 +51,6 @@ async def help_(message: types.Message): # В параметры функции 
 @dp.message_handler(commands=['support']) # Вызов поддержки
 async def support_(message: types.Message):# В параметры функции попадает событие сообщения из чата
     await message.answer(text=s_c) # await ждёт пока в потоке появится свободное место
-    await message.delete() # await ждёт пока в потоке появится свободное место
-@dp.message_handler(commands=['secret']) # Вызов поддержки
-async def support_(message: types.Message): # В параметры функции попадает событие сообщения из чата
-    await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJZDpkjyiCq7JzLczhnaE7yRp7noNrawACQx0AAmt1-UkOzn3LS4oOBi8E') # await ждёт пока в потоке появится свободное место
     await message.delete() # await ждёт пока в потоке появится свободное место
 @dp.message_handler() # автоматический ответ
 async def auto_uns_(message: types.Message): # В параметры функции попадает событие сообщения из чата
